cbfd762fbee5096b24922db6298fe480c3f1a9af
swapnil265 <rajswapnil265@gmail.com>
2022-04-27 23:48:20 -0400

    Book CRUD start day1

---
+++ BookMS/src/test/java/com/amazingBooks/BookMS/BookMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazingBooks.BookMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
