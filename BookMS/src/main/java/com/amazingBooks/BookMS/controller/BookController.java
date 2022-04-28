37d57ea6280a5becaf2874ad657388c759e9642d
swapnil265 <rajswapnil265@gmail.com>
2022-04-27 23:44:35 -0400

    Book CRUD start day1

--- BookMS/src/main/java/com/amazingBooks/BookMS/controller/BookController.java
+++ BookMS/src/main/java/com/amazingBooks/BookMS/controller/BookController.java
@@ -1,6 +1,17 @@
 package com.amazingBooks.BookMS.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.awt.print.Book;
+
+@RequestMapping("/Books")
 public class BookController {
 
+@GetMapping()
+    String testBooksController(){
+
+    return "Hello I am Book Contoller";
+}
 
 }
